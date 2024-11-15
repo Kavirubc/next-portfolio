998126c5bf158c498b6f7a5617c6e7656c13891f
kaviru <hapuarachchikaviru@gmail.com>
2024-11-15 12:22:08 +0530

    Update footer links and copyright information

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,26 +6,26 @@ const Footer = () => {
             <div className="container mx-auto px-6 py-9">
                 <div className="flex flex-col mt-4">
                     <div className="flex flex-row space-x-4 justify-center">
-                        <Link href="/gallery">
+                        {/* <Link href="/gallery">
                             <div className="text-sm hover:text-black dark:hover:text-white text-gray-400 dark:text-gray-500 text-center">Gallery</div>
-                        </Link>
+                        </Link> */}
                         <Link href="/contact">
                             <div className="text-sm hover:text-black dark:hover:text-white text-gray-400 dark:text-gray-500 text-center">Contact Me</div>
                         </Link>
-                        <Link target="_blank" href="https://www.ko-de.org/">
+                        {/* <Link target="_blank" href="https://www.ko-de.org/">
                             <div className="text-sm hover:text-black dark:hover:text-white text-gray-400 dark:text-gray-500 text-center">Projects</div>
-                        </Link>
+                        </Link> */}
                     </div>
                     <div className="flex flex-row justify-center my-10 space-x-4">
-                        <Link target="_blank" href="https://www.ko-de.org/">
-                            <div className="text-sm hover:text-black dark:hover:text-white text-gray-400 dark:text-gray-500 text-center">kode</div>
+                        <Link target="_blank" href="https://www.linkedin.com/in/kaviruhapuarachchi/">
+                            <div className="text-sm hover:text-black dark:hover:text-white text-gray-400 dark:text-gray-500 text-center">LinkedIn</div>
                         </Link>
                         <Link target="_blank" href="https://github.com/Kavirubc">
                             <div className="text-sm hover:text-black dark:hover:text-white text-gray-400 dark:text-gray-500 text-center">Github</div>
                         </Link>
                     </div>
                     <p className="text-sm text-slate-300 dark:text-slate-500 hover:text-indigo-300 dark:hover:text-indigo-500 text-center">
-                        © {new Date().getFullYear()} kode. All rights reserved.
+                        © {new Date().getFullYear()} kaviruh. All rights reserved.
                     </p>
                 </div>
             </div>
